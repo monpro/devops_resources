35d271df35f3663e3c128799ab09920dfb6d663c
monpro <monproshen@gmail.com>
2020-09-15 23:19:33 +1000

    update module example using 3rd party lib from git

---
+++ module/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+  default = "ap-southeast-2"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
