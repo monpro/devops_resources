c02725df292b3f26cc1a805531152e5accee4725
monpro <monproshen@gmail.com>
2020-09-17 21:51:44 +1000

    add ebs volume for the ec2 instance

--- vpc/instance.tf
+++ vpc/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "vpc_example" {
+resource "aws_instance" "vpc-example" {
     ami = var.AMIS[var.AWS_REGION]
     instance_type = "t2.micro"
 
@@ -10,4 +10,19 @@ resource "aws_instance" "vpc_example" {
 
     # set the public ssh key, then you could use the private key to ssh
     key_name = aws_key_pair.vpc-test-key.key_name
+}
+
+resource "aws_ebs_volume" "ebs-volume-1" {
+    availability_zone = "${var.AWS_REGION}a"
+    size = 20
+    type = "gp2"
+    tags = {
+        Name = "extra volume data"
+    }
+}
+
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+    device_name = "/dev/xvdh"
+    volume_id = aws_ebs_volume.ebs-volume-1.id
+    instance_id = aws_instance.vpc-example.id
 }
