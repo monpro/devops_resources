c619d0830da2674f478e1480470f6aa4ecc09fed
monpro <monproshen@gmail.com>
2021-03-02 23:01:02 +1100

    uploading files onto s3 using cdk and let lambda function fetch the name of corresponding bucket

--- cdk/sample-app/api/get-photos/index.ts
+++ cdk/sample-app/api/get-photos/index.ts
@@ -1,8 +1,9 @@
 import {APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context} from "aws-lambda";
 
+const bucketName = process.env.PHOTO_BUCKET_NAME;
 export const getPhotos = async (event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyResultV2> => {
   return {
     statusCode: 200,
-    body: 'test msg from lambda function'
+    body: `test msg from lambda function, the bucket name is ${bucketName}`
   }
 };
--- cdk/sample-app/lib/sample-app-stack.ts
+++ cdk/sample-app/lib/sample-app-stack.ts
@@ -3,6 +3,7 @@ import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 import {Bucket, BucketEncryption} from "@aws-cdk/aws-s3";
 import {Runtime} from "@aws-cdk/aws-lambda";
 import * as path from "path";
+import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
 
 export class SampleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,10 +13,20 @@ export class SampleAppStack extends cdk.Stack {
       encryption: BucketEncryption.S3_MANAGED
     });
 
+    new BucketDeployment(this, 'SampleAppPhotos', {
+      sources: [
+        Source.asset(path.join(__dirname, '..', 'photos'))
+      ],
+      destinationBucket: bucket
+    });
+
     const getPhotos = new lambda.NodejsFunction(this, 'SampleLambda', {
       runtime: Runtime.NODEJS_12_X,
       entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
-      handler: 'getPhotos'
+      handler: 'getPhotos',
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName
+      }
     });
 
     new cdk.CfnOutput(this, 'SampleBucketNameExport', {
