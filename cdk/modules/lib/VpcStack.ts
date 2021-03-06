249c2db2ce39d096d790bbad118b889619ecf6d1
monpro <monproshen@gmail.com>
2021-03-06 18:40:21 +1100

    use system manager to store the private subnet id

--- cdk/modules/lib/VpcStack.ts
+++ cdk/modules/lib/VpcStack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ssm from '@aws-cdk/aws-ssm';
 import {SubnetType} from '@aws-cdk/aws-ec2';
 
 interface VpcStackProps extends cdk.StackProps {
@@ -7,11 +8,11 @@ interface VpcStackProps extends cdk.StackProps {
 }
 
 export class VpcStack extends cdk.Stack {
-  private readonly vpc;
+  private readonly vpc: ec2.Vpc;
 
   constructor(scope: cdk.Construct, id: string, props: VpcStackProps) {
     super(scope, id, props);
-
+    const {envName} = props;
     this.vpc = new ec2.Vpc(this, 'devVpc', {
       cidr: '156.32.0.0/16',
       maxAzs: 2,
@@ -35,6 +36,15 @@ export class VpcStack extends cdk.Stack {
         }
       ],
       natGateways: 1
+    });
+
+    const privateSubnetsIds = this.vpc.privateSubnets.map(subnet => subnet.subnetId);
+
+    privateSubnetsIds.forEach((subnetId, index) => {
+      new ssm.StringParameter(this, `private-subnet-${index}`, {
+        parameterName: `/${envName}/private-subnet-${index}`,
+        stringValue: subnetId
+      })
     })
   }
 }
