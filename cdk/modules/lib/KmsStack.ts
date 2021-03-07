283740eecf5b7b48ce75f6279971b7b5955df6a5
monpro <monproshen@gmail.com>
2021-03-07 12:20:37 +1100

    add KmsStack to generate encryption keys

---
+++ cdk/modules/lib/KmsStack.ts
@@ -0,0 +1,38 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as kms from '@aws-cdk/aws-kms';
+import * as ssm from '@aws-cdk/aws-ssm';
+
+interface KmsStackProps extends cdk.StackProps {
+  envName: string;
+  keyDesc: string;
+  keyId: string;
+  keyAlias: string;
+}
+
+export class KmsStack extends cdk.Stack {
+  private readonly kmsKey: kms.Key;
+
+  constructor(scope: cdk.Construct, id: string, props: KmsStackProps ) {
+    super(scope, id, props);
+
+    const {
+      envName,
+      keyId,
+      keyDesc,
+      keyAlias
+    } = props;
+
+    this.kmsKey = new kms.Key(this, keyId, {
+      description: keyDesc,
+      enableKeyRotation: true
+    });
+
+    this.kmsKey.addAlias(keyAlias);
+
+    new ssm.StringParameter(this, `${keyId}/${keyDesc}`, {
+      parameterName: `/${envName}/${keyId}`,
+      stringValue: this.kmsKey.keyId
+    })
+  }
+}
