9b0f3528a6f423251c9f426b6dc81fcc23d0fa7b
monpro <monproshen@gmail.com>
2021-03-07 12:22:18 +1100

    add BastionHostStack module to create bastion host to get access to public ec2 instances

--- cdk/modules/bin/modules.ts
+++ cdk/modules/bin/modules.ts
@@ -3,12 +3,33 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { VpcStack } from '../lib/VpcStack';
 import {SecurityStack} from "../lib/SecurityStack";
+import {BastionHostStack} from "../lib/BastionHostStack";
+import {KmsStack} from "../lib/KmsStack";
 
 const app = new cdk.App();
 const {vpc} = new VpcStack(app, 'VpcStack', {
   envName: 'dev'
 });
 
-new SecurityStack(app, 'SecurityStack', vpc, {
+const {bastionSg} = new SecurityStack(app, 'SecurityStack', vpc, {
   envName: 'dev'
+});
+
+new KmsStack(app, 'monCdkKey', {
+  envName: 'dev',
+  keyId: 'monCdk',
+  keyDesc: 'cdk key',
+  keyAlias: 'alias/monCdk'
+});
+
+new KmsStack(app, 'rdsKey', {
+  envName: 'dev',
+  keyId: 'rdsCdk',
+  keyDesc: 'rds key',
+  keyAlias: 'alias/rdsCdk'
+});
+
+
+new BastionHostStack(app, 'BastionHostStack', vpc, bastionSg, {
+  keyName: 'monCdkKey'
 });
---
+++ cdk/modules/lib/BastionHostStack.ts
@@ -0,0 +1,31 @@
+import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+
+interface BastionHostStackProps extends cdk.StackProps {
+  keyName: string;
+}
+export class BastionHostStack extends cdk.Stack {
+  private readonly bastionHost: ec2.Instance;
+
+  constructor(scope: cdk.Construct, id: string, vpc: ec2.Vpc, sg: ec2.SecurityGroup, props: BastionHostStackProps ) {
+    super(scope, id, props);
+
+    const {keyName} = props;
+    this.bastionHost = new ec2.Instance(this, 'bastionHost', {
+      vpc,
+      keyName,
+      securityGroup: sg,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage({
+        edition: ec2.AmazonLinuxEdition.STANDARD,
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        virtualization: ec2.AmazonLinuxVirt.HVM,
+        storage: ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+      }),
+      // make bastion host works on public subnet
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC
+      }
+    })
+  }
+}
