22093ad06391b01cbaced8db6399dd51b509e903
monpro <monproshen@gmail.com>
2021-03-09 13:49:43 +1100

    add cognito user pool and user pool client using cdk

---
+++ cdk/modules/lib/CognitoStack.ts
@@ -0,0 +1,47 @@
+import * as cdk from '@aws-cdk/core';
+import * as cognito from '@aws-cdk/aws-cognito';
+
+interface CognitoStackProps extends cdk.StackProps {
+  envName: string;
+}
+
+export class CognitoStack extends cdk.Stack {
+
+  private readonly userPool: cognito.CfnUserPool;
+  private readonly userPoolClient: cognito.CfnUserPoolClient;
+  private readonly identityPool: cognito.CfnIdentityPool;
+
+  constructor(scope: cdk.Construct, id: string, props: CognitoStackProps) {
+    super(scope, id, props);
+    const {envName} = props;
+
+    this.userPool = new cognito.CfnUserPool(this, 'monCognitoUserPool', {
+      autoVerifiedAttributes: ['email'],
+      usernameAttributes: ['email', 'phone_number'],
+      userPoolName: 'monUserPool',
+      schema: [
+        {
+          attributeDataType: 'String',
+          name: 'param',
+          mutable: true
+        }
+      ],
+      policies: {
+        passwordPolicy: {
+          minimumLength: 12,
+          requireLowercase: true,
+          requireNumbers: true,
+          requireSymbols: false,
+          requireUppercase: true
+        }
+      }
+    });
+
+    this.userPoolClient = new cognito.CfnUserPoolClient(this, 'monPoolClient', {
+      userPoolId: this.userPool.ref,
+      clientName: `${envName}AppClient`
+    });
+
+
+  }
+}
